f49f345dbe5c167ef6fbdd243638e4101ed2ddef
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-03-30 23:41:40 -0400

    Add files via upload

--- litepolis_database_template/Actor.py
+++ litepolis_database_template/Actor.py
@@ -4,4 +4,15 @@ from .Users import UserManager
 from .Conversations import ConversationManager
 
 class DatabaseActor(UserManager, ConversationManager):
+    """
+    DatabaseActor class for LitePolis.
+
+    This class serves as the central point of interaction between the LitePolis system
+    and the database module. It aggregates operations from various manager classes,
+    such as UserManager and ConversationManager, providing a unified interface
+    for database interactions.
+
+    LitePolis system is designed to interact with a class named "DatabaseActor",
+    so ensure this class name is maintained.
+    """
     pass
